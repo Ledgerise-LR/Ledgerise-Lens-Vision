c9802fdce6759c9452f8e4e5e883397565312872
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2023-09-06 21:56:52 +0300

    model_builder

---
+++ model_builder.py
@@ -0,0 +1,55 @@
+import torch
+from torch import nn
+
+
+class LedgeriseLens(nn.Module):
+    def __init__(self, input_channels, hidden_units, output_channels):
+        super().__init__()
+
+        self.conv_layer_1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=input_channels,
+                out_channels=hidden_units,
+                kernel_size=(3, 3),
+                padding=1,
+                stride=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=(3, 3),
+                padding=1,
+                stride=1,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+        )
+
+        self.conv_layer_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=(3, 3),
+                padding=0,
+                stride=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=(3, 3),
+                padding=0,
+                stride=1,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units, out_features=output_channels),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.classifier(self.conv_layer_2(self.conv_layer_1(x)))
